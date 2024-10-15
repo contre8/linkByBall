297260d5902ca30a1d10cabac17c48ebbd193076
Adri8Typsa <acontreras@typsa.es>
2024-10-15 20:15:37 +0200

    Generalización de favoritos

---
+++ src/app/service/favoritos/favoritos.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FavoritosService {
+  private baseUrl = 'http://localhost:8000'; // Ajusta la URL si es necesario
+
+  constructor(private http: HttpClient) { }
+
+  // Añadir favorito
+  addFavorite(favoriteId: string, userType: string): Observable<any> {
+    return this.http.post(`${this.baseUrl}/favoritos`, { favoriteId, userType });
+  }
+
+  // Eliminar favorito
+  removeFavorite(favoriteId: string, userType: string): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/favoritos/${favoriteId}/${userType}`);
+  }
+
+  // Obtener favoritos
+  getFavorites(userId: string, userType: string): Observable<any> {
+    return this.http.get(`${this.baseUrl}/favoritos/${userId}/${userType}`);
+
+  }
+
+  // Verificar si un ítem es favorito
+  isFavorite(favoriteId: string, userType: string): Observable<any> {
+    return this.http.get(`${this.baseUrl}/favoritos/isFavorite/${favoriteId}/${userType}`);
+  }
+
+}
